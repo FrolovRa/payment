1aaffc2e83dd4a7c448b44504201a154e1962ead
frolovroman <frorom@icloud.com>
2021-09-19 14:39:08 +0300

    #master init commit - init.sql script for tables - docker configuration

---
+++ db/init.sql
@@ -0,0 +1,56 @@
+create table iban
+(
+    id   bigint             not null auto_increment primary key,
+    iban varchar(34) unique not null
+);
+
+create table user
+(
+    id       bigint              not null auto_increment primary key,
+    iban_id  bigint              not null,
+    name     varchar(255) unique not null,
+    password varchar(255)        not null,
+
+    constraint fk__user__iban foreign key (iban_id) references iban (id)
+);
+
+create table payment_type
+(
+    id              bigint             not null auto_increment primary key,
+    payment_type    varchar(10) unique not null,
+    fee_coefficient decimal(4, 3)
+);
+
+create table currency
+(
+    id       bigint             not null auto_increment primary key,
+    currency varchar(10) unique not null
+);
+
+create table bic_code
+(
+    id       bigint             not null auto_increment primary key,
+    bic_code varchar(11) unique not null
+);
+
+create table payment
+(
+    id                    bigint    not null auto_increment primary key,
+    creditor_iban_id      bigint    not null,
+    debtor_iban_id        bigint    not null,
+    payment_type_id       bigint    not null,
+    currency_id           bigint    not null,
+    bic_code_id           bigint    not null,
+    cancel_for_payment_id bigint,
+    amount                numeric   not null,
+    details               varchar(255),
+    created_at            timestamp not null,
+
+    constraint un__creditor_iban__debtor_iban unique (creditor_iban_id, debtor_iban_id),
+    constraint fk__payment__creditor_iban foreign key (creditor_iban_id) references iban (id),
+    constraint fk__payment__debtor_iban foreign key (debtor_iban_id) references iban (id),
+    constraint fk__payment__payment_type foreign key (payment_type_id) references payment_type (id),
+    constraint fk__payment__currency foreign key (currency_id) references currency (id),
+    constraint fk__payment__bic_code foreign key (bic_code_id) references bic_code (id),
+    constraint fk__payment__payment foreign key (cancel_for_payment_id) references payment (id)
+);
---
+++ src/main/java/com/crud/payment/PaymentCrudApplication.java
@@ -0,0 +1,13 @@
+package com.crud.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentCrudApplication.class, args);
+    }
+
+}
